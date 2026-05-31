c30a5a6e6f1ab714cb9b59be27da039410af37cd
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-05-31 11:37:32 +0530

    feat: add product model, mock product data and images

---
+++ src/app/features/products/data/products.ts
@@ -0,0 +1,54 @@
+import { Product } from '../models/product';
+
+export const PRODUCTS: Product[] = [
+  {
+    "id": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2025",
+    "description": "Leaf rake with 48-inch hardwood handle.",
+    "price": 21.95,
+    "starRating": 3.8,
+    "imageUrl": "images/leaf_rake.png"
+  },
+  {
+    "id": 2,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2025",
+    "description": "20-gallon heavy-duty rolling garden cart.",
+    "price": 39.99,
+    "starRating": 4.4,
+    "imageUrl": "images/garden_cart.png"
+  },
+  {
+    "id": 3,
+    "productName": "Hammer",
+    "productCode": "TBX-0048",
+    "releaseDate": "May 21, 2025",
+    "description": "16 oz curved claw steel hammer.",
+    "price": 10.99,
+    "starRating": 4.8,
+    "imageUrl": "images/hammer.png"
+  },
+  {
+    "id": 4,
+    "productName": "Saw",
+    "productCode": "TBX-0022",
+    "releaseDate": "May 15, 2025",
+    "description": "15-inch steel blade hand saw.",
+    "price": 13.95,
+    "starRating": 3.9,
+    "imageUrl": "images/saw.png"
+  },
+  {
+    "id": 5,
+    "productName": "Video Game Controller",
+    "productCode": "GMG-0042",
+    "releaseDate": "October 15, 2025",
+    "description": "Wireless multi-platform game controller.",
+    "price": 39.95,
+    "starRating": 4.7,
+    "imageUrl": "images/xbox-controller.png"
+  }
+];
---
+++ src/app/features/products/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    description: string;
+    price: number;
+    starRating: number;
+    imageUrl: string;
+}
