16bb5593a1fa6a96e1c9aca5248c2a273f131be1
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-06-20 09:33:32 +0530

    feat(products): add product domain model and create product service

---
+++ src/app/features/products/services/product.service.ts
@@ -0,0 +1,20 @@
+import { IProduct } from '../models/product.model';
+
+export class ProductService {
+  products: IProduct[] = [
+    {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2021',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    },
+  ];
+
+  getProducts(): IProduct[] {
+    return this.products;
+  }
+}
