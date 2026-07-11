9fc719ac69b73c8edf81d59e463235f2e37b49a4
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-07-11 17:06:53 +0530

    chore: upgrade Angular from v20 to v21

--- src/app/features/home/pages/home-page/home-page.ts
+++ src/app/features/home/pages/home-page/home-page.ts
@@ -1,15 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { HomeService } from '../../services/home.service';
-import { CommonModule } from '@angular/common';
+
 import { DashboardCard } from '../../components/dashboard-card/dashboard-card';
 import { QuickAction } from '../../components/quick-action/quick-action';
 import { PAGE_SUBTITLE, PAGE_TITLE } from '../../constants/home.constants';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule,
-    DashboardCard,
-    QuickAction],
+  imports: [DashboardCard, QuickAction],
   templateUrl: './home-page.html',
 })
 export class HomePage {
